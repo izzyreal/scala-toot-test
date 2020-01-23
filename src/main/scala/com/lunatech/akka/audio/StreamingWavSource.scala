c125406943abdb21ab18ffbd162fe932ad65f78b
Izmar Verhage <izmar.verhage@lunatech.nl>
2020-01-23 17:35:44 +0100

    Add breakbeat

--- src/main/scala/com/lunatech/akka/audio/StreamingWavSource.scala
+++ src/main/scala/com/lunatech/akka/audio/StreamingWavSource.scala
@@ -1,31 +1,26 @@
 package com.lunatech.akka.audio
 
+import java.io.File
+
 import akka.stream.stage.{GraphStage, GraphStageLogic, OutHandler}
 import akka.stream.{Attributes, Outlet, SourceShape}
+import uk.co.labbookpages.WavFile
 
 class StreamingWavSource extends GraphStage[SourceShape[Double]] {
 
-  private val Freq = 440
-  private val SampleRate = 44100
-  private val TwoPi = 2 * Math.PI
+  val wavFile = WavFile.openWavFile(new File("breakbeat.wav"))
+  val numChannels = wavFile.getNumChannels
+  val buffer = new Array[Double](numChannels)
 
   val out: Outlet[Double] = Outlet("StreamingWavSource")
   override val shape: SourceShape[Double] = SourceShape(out)
 
   override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
     new GraphStageLogic(shape) {
-
-      private var frameIndex = 0
-
       setHandler(out, new OutHandler {
         override def onPull(): Unit = {
-          if (frameIndex % 88200 > 10000) {
-            push(out, 0d)
-          } else {
-            val sample = Math.sin((TwoPi * Freq) / SampleRate * frameIndex)
-            push(out, sample)
-          }
-          frameIndex += 1
+          wavFile.readFrames(buffer, 1)
+          push(out, buffer(0))
         }
       })
     }
